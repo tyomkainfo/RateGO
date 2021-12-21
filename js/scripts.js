5c4dc46731ef4e3922d2432f55966369322ac616
rocknant <vitaliykudrya92@gmail.com>
2021-12-22 01:14:42 +0200

    add section slider not end

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-});
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:2
+            },
+
+            1000:{
+                items:2
+            }
+        }
+    });
+});
 
 
 
